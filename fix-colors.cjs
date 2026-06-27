e7e3c25098b19abbd8cb12f8b5feb3d5a3202a3c
vietndj <vietndj@gmail.com>
2026-06-27 21:01:27 +0700

    add ugc.html and update build script

--- fix-colors.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/App.jsx', 'utf8');
-
-content = content.replace(/state\.db\.colors\[k\]/g, "(state.db.colors || {})[k]");
-content = content.replace(/state\.db\.files\.filter/g, "(state.db.files || []).filter");
-content = content.replace(/\[\.\.\.state\.db\.files\]/g, "[...(state.db.files || [])]");
-
-fs.writeFileSync('src/App.jsx', content);
-console.log("Fixed colors and files in App.jsx");
